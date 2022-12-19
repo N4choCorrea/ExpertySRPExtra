edc79b36659a53bc22b38970e55662e9718d0b47
N4choCorrea <correa.ignacio48@gmail.com>
2022-12-19 09:21:42 -0300

    Hice deafio 5

--- src/Library/FilePrinter.cs
+++ src/Library/FilePrinter.cs
@@ -5,7 +5,7 @@ namespace Full_GRASP_And_SOLID.Library
 {
     public class FilePrinter : IPrinter
     {
-        public void PrintBuilding(Building building)
+        public void PrintBuilding(IPrintable building)
         {
             File.WriteAllText("Building.txt", building.PrintBuilding());
         }
--- src/Library/IPrinter.cs
+++ src/Library/IPrinter.cs
@@ -2,6 +2,6 @@ namespace Full_GRASP_And_SOLID.Library
 {
     public interface IPrinter
     {
-        void PrintBuilding(Building building);
+        void PrintBuilding(IPrintable printable);
     }
 }
