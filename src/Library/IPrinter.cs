e79b051c9bd69c1b82eba88eb3df9f367eb64973
N4choCorrea <correa.ignacio48@gmail.com>
2022-12-22 22:37:04 -0300

    comentando el patron

--- src/Library/IPrinter.cs
+++ src/Library/IPrinter.cs
@@ -4,4 +4,8 @@ namespace Full_GRASP_And_SOLID.Library
     {
         void PrintBuilding(Building building);
     }
+    //Aplicando esta interfaz se puede imprimir en consola o en un archivo, por lo que aplicamos el patron poliformismo
+    //porque la clase ConsolePrinter y FilePrinter implementan la interfaz IPrinter, y por tanto aplicamos operaciones 
+    //polimorficas ya que su uso puede variar y si quisieramos cambiar algo ahora solo tendriamos que agregar una nueva clase 
+    //que implemente la interfaz IPrinter sin tener que modificar las clases que ya estan implementadas
 }
