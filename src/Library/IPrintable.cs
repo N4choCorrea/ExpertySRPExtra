8b37551e82410e19bd312076d3ddf2ec1c145e56
N4choCorrea <correa.ignacio48@gmail.com>
2022-12-22 23:10:12 -0300

    patron comentado

--- src/Library/IPrintable.cs
+++ src/Library/IPrintable.cs
@@ -6,3 +6,7 @@ namespace Full_GRASP_And_SOLID.Library
         string PrintBuilding();
     }
 }
+// Aqui aplicamos el patron DIP, debido a que creamos esta interfaz para que la clases dependan 
+// de una abstraccion para que una clase no dependa de una clase concreta, sino que dependa de una abstraccion
+//ahora podemos agregar cambios en las diferentes clases sin afectar a las demas clases, por ejemplo building,
+// ya que ahora unas no depende de la otra, sino que depende de una abstraccion, en este caso la interfaz IPrintable
